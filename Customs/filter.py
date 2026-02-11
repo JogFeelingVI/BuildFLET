538678b9f1d25ac5d4f5f75d09132d598a202d07
JogfeeingVI <lifelse@outlook.com>
2026-02-11 10:13:12 +0800

    更新 filter.py

--- Customs/filter.py
+++ Customs/filter.py
@@ -4,8 +4,6 @@
 # @Last Modified by:   JogFeelingVI
 # @Last Modified time: 2026-02-10 02:42:15
 
-
-from turtle import bgcolor
 from .ColorTokenizer import Tokenizer, spiltfortarget
 from .jackpot_core import filterFunc
 from .SnackBar import get_snack_bar
