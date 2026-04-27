52c825dc9e735c0262a8f43ced6a06ee7d67b4b2
JogfeeingVI <lifelse@outlook.com>
2026-04-27 20:39:10 +0800

    fix include

--- Customs/setings.py
+++ Customs/setings.py
@@ -2,9 +2,11 @@
 # @Author: JogFeelingVI
 # @Date:   2025-12-28 00:32:47
 # @Last Modified by:   JogFeelingVI
-# @Last Modified time: 2026-04-26 14:22:51
+# @Last Modified time: 2026-04-22 13:05:11
 
 import asyncio
+import os
+import pathlib
 import re
 import json
 
@@ -757,7 +759,7 @@ class rsup(ft.Container):
             upstash.bgcolor = ft.Colors.with_opacity(0.3, upbgc)
             upstash.border = ft.Border.all(1, ft.Colors.with_opacity(0.4, upbgc))
         # 测试对话框
-        _ = ft.Container(
+        testadb = ft.Container(
             padding=ft.Padding(10, 5, 10, 5),
             alignment=ft.Alignment.CENTER,
             border_radius=8,
